11a9f65cf29eed5c411adcc1d83d02ffccda23b3
philopon <philopon.dependence@gmail.com>
2014-12-28 01:27:24 +0900

    reduce indent

--- src/Test/Assert/Simple.purs
+++ src/Test/Assert/Simple.purs
@@ -9,59 +9,59 @@ module Test.Assert.Simple
   , assertThrows
   ) where
 
-  import Control.Monad.Eff
-  import Control.Monad.Eff.Exception
-  import Test.Assert.AssertionError
-  import Data.Maybe
-  import Data.String
-  import Data.Function
-  import Data.Foldable
+import Control.Monad.Eff
+import Control.Monad.Eff.Exception
+import Test.Assert.AssertionError
+import Data.Maybe
+import Data.String
+import Data.Function
+import Data.Foldable
 
-  type Assertion e = Eff (err :: Exception | e) Unit
+type Assertion e = Eff (err :: Exception | e) Unit
 
-  when :: forall e. Boolean -> Eff e Unit -> Eff e Unit
-  when b a = if b then a else return unit
+when :: forall e. Boolean -> Eff e Unit -> Eff e Unit
+when b a = if b then a else return unit
 
-  unless :: forall e. Boolean -> Eff e Unit -> Eff e Unit
-  unless b = when (not b)
+unless :: forall e. Boolean -> Eff e Unit -> Eff e Unit
+unless b = when (not b)
 
-  assertSuccess :: forall e. Assertion e
-  assertSuccess = return unit
+assertSuccess :: forall e. Assertion e
+assertSuccess = return unit
 
-  assertFailure :: forall e. String -> Assertion e
-  assertFailure msg =
-    throwException (toError $ assertionError msg {} Nothing)
+assertFailure :: forall e. String -> Assertion e
+assertFailure msg =
+  throwException (toError $ assertionError msg {} Nothing)
 
-  assertBool :: forall e. String -> Boolean -> Assertion e
-  assertBool msg b = unless b (assertFailure msg)
+assertBool :: forall e. String -> Boolean -> Assertion e
+assertBool msg b = unless b (assertFailure msg)
 
-  assertEqual :: forall e a. (Eq a, Show a) => a -> a -> Assertion e
-  assertEqual expected actual =
-    unless (actual == expected) (assertFailure msg)
-      where msg = "expected: " ++ show expected ++
-                  " but got: " ++ show actual
+assertEqual :: forall e a. (Eq a, Show a) => a -> a -> Assertion e
+assertEqual expected actual =
+  unless (actual == expected) (assertFailure msg)
+    where msg = "expected: " ++ show expected ++
+                " but got: " ++ show actual
 
-  assertString :: forall e. String -> Assertion e
-  assertString s = unless (null s) (assertFailure s)
+assertString :: forall e. String -> Assertion e
+assertString s = unless (s == "") (assertFailure s)
 
-  assertIn :: forall e a. (Eq a, Show a) => [a] -> a -> Assertion e
-  assertIn expecteds actual =
-    unless (any ((==) actual) expecteds) (assertFailure msg)
-      where msg = "expected: " ++ joinWith " | " (show <$> expecteds) ++
-                  " but got: " ++ show actual
+assertIn :: forall e a. (Eq a, Show a) => [a] -> a -> Assertion e
+assertIn expecteds actual =
+  unless (any ((==) actual) expecteds) (assertFailure msg)
+    where msg = "expected: " ++ joinWith " | " (show <$> expecteds) ++
+                " but got: " ++ show actual
 
-  (@=?) :: forall e a. (Eq a, Show a) => a -> a -> Assertion e
-  (@=?) expected actual = assertEqual expected actual
+(@=?) :: forall e a. (Eq a, Show a) => a -> a -> Assertion e
+(@=?) expected actual = assertEqual expected actual
 
-  (@?=) :: forall e a. (Eq a, Show a) => a -> a -> Assertion e
-  (@?=) actual expected = assertEqual expected actual
+(@?=) :: forall e a. (Eq a, Show a) => a -> a -> Assertion e
+(@?=) actual expected = assertEqual expected actual
 
-  infix 1 @=?
-  infix 1 @?=
+infix 1 @=?
+infix 1 @?=
 
-  data ThrowsResult a = NotThrows a | Ok | NG Error
+data ThrowsResult a = NotThrows a | Ok | NG Error
 
-  foreign import assertThrowsImpl """
+foreign import assertThrowsImpl """
 function assertThrowsImpl(r, p, m){
   function assertThrowsImpl_Eff(){
     try {
@@ -83,11 +83,11 @@ function assertThrowsImpl(r, p, m){
         (Eff (err :: Exception | e) a) 
         (Eff (err :: Exception | e) b) 
 
-  assertThrows :: forall e a. (Error -> Boolean)
-               -> (Eff (err :: Exception | e) a) -> Assertion e
-  assertThrows p m = do
-    r <- runFn3 assertThrowsImpl {notThrows: NotThrows, ok: Ok, ng: NG} p m
-    case r of
-         NotThrows _ -> assertFailure "error not threw"
-         Ok          -> assertSuccess
-         NG        e -> assertFailure $ "not expected error is threw: " ++ show e
+assertThrows :: forall e a. (Error -> Boolean)
+             -> (Eff (err :: Exception | e) a) -> Assertion e
+assertThrows p m = do
+  r <- runFn3 assertThrowsImpl {notThrows: NotThrows, ok: Ok, ng: NG} p m
+  case r of
+       NotThrows _ -> assertFailure "error not threw"
+       Ok          -> assertSuccess
+       NG        e -> assertFailure $ "not expected error is threw: " ++ show e
